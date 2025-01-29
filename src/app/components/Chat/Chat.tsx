9031d09758e413afd4c764354e8d97f2bced2d90
Tyler Knight <tknightx31@gmail.com>
2025-01-29 16:20:06 -0600

    feat: main chat component

--- src/app/components/Chat/Chat.tsx
+++ src/app/components/Chat/Chat.tsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { useChat } from "ai/react";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: "/api/openai",
+  });
+
+  const chatContainerRef = useRef<HTMLDivElement>(null);
+
+  // keep the chat in the viewport
+  const scroll = () => {
+    const { offsetHeight, scrollHeight, scrollTop } =
+      chatContainerRef.current as HTMLDivElement;
+
+    if (scrollHeight >= offsetHeight + scrollTop) {
+      chatContainerRef.current?.scrollTo(0, scrollHeight + 200);
+    }
+  };
+
+  useEffect(() => {
+    scroll();
+  }, [messages]);
+
+  const renderResponse = () => {
+    return (
+      <div>
+        {messages.map((m, i) => {
+          return (
+            <div key={m.id}>
+              <p>{m.content}</p>
+              {i < messages.length - 1 && <hr />}
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  return (
+    <div ref={chatContainerRef}>
+      {renderResponse()}
+      <form onSubmit={handleSubmit}>
+        <input
+          name="input-field"
+          type="text"
+          placeholder="say something"
+          value={input}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 };
 
 export default Chat;
